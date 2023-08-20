8e8e3ca51c32f5612bfffc931130ce0449131832
jaeil.joo <sgsgself@naver.com>
2023-08-20 18:29:25 +0900

    Time: 51 ms (77.86%), Space: 42.1 MB (55.00%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,8 +12,17 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
+    const stack = [[p, q]];
     
-    if (!p && !q) return true;
-    if (!p || !q) return false;
-    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    while (stack.length > 0) {
+        const [node1, node2] = stack.pop();
+        
+        if (!node1 && !node2) continue;
+        if (!node1 || !node2 || node1.val !== node2.val) return false;
+        
+        stack.push([node1.left, node2.left]);
+        stack.push([node1.right, node2.right]);
+    }
+    
+    return true;
 };
